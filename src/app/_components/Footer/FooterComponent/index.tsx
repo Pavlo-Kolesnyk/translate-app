aafad8acbbe61ef380b1836f11ee95c525462ab0
Pavlo-Kolesnyk <pavlo.kolesnyk.dev@gmail.com>
2024-01-31 20:56:18 +0200

    1.38.38 new key2

--- src/app/_components/Footer/FooterComponent/index.tsx
+++ src/app/_components/Footer/FooterComponent/index.tsx
@@ -43,7 +43,7 @@ const FooterComponent = ({footer}: {footer: Footer}) => {
               <Image src="/polyglot.svg" alt="logo" width={170} height={50} />
             </Link>
 
-            <p>{footer.copyright}</p>
+            {/* <p>{footer.copyright}</p> */}
 
             <div className={classes.socialLinks}>
               {navItems.map((item) => {
